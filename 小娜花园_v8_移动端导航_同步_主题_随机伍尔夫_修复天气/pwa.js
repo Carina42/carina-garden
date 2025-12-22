7978c3111716c96fcc966e555ab67737130eb884
Carina42 <103558982+Carina42@users.noreply.github.com>
2025-12-22 17:57:18 +0800

    Delete 小娜花园_v8_移动端导航_同步_主题_随机伍尔夫_修复天气 directory

--- 小娜花园_v8_移动端导航_同步_主题_随机伍尔夫_修复天气/pwa.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  if(!("serviceWorker" in navigator)) return;
-  window.addEventListener("load", ()=>{
-    navigator.serviceWorker.register("./sw.js").catch(()=>{});
-  });
-})();
--- 小娜花园_v8_移动端导航_同步_主题_随机伍尔夫_修复天气/quoteInit.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  document.addEventListener("DOMContentLoaded", ()=>{
-    try{
-      if(window.WoolfQuotes && window.WoolfQuotes.render){
-        window.WoolfQuotes.render("woolfQuote","woolfQuoteSource");
-      }
-    }catch(_){}
-  });
-})();
